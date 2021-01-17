70362a28b417e27feb308fd0db8e0bdfdac97014
GGLuks <64063689+GGLuks@users.noreply.github.com>
2021-01-18 02:01:59 +0700

    model lokasi

---
+++ app/src/main/java/com/example/tubes03_a/Lokasi.java
@@ -0,0 +1,19 @@
+package com.example.tubes03_a;
+
+public class Lokasi {
+    private String lokasi;
+    public Lokasi(){}
+
+    //Constructor
+    public Lokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+}
